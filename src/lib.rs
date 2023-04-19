78ae6534bfe3fda4c6e5be2c929bac1345626af4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-04-19 08:37:09 +0700

    📦 Added unit test

--- src/lib.rs
+++ src/lib.rs
@@ -112,3 +112,41 @@ impl Drop for ThreadPool {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crossbeam_channel::unbounded;
+
+    use crate::{ThreadPool, ThreadPoolError};
+
+    /// Test the crossbeam thread pooling implementation
+    ///
+    /// ## Panic
+    ///
+    /// It may panic if the OS cannot create a thread
+    #[test]
+    fn test_crossbeam() -> Result<(), ThreadPoolError> {
+        let pool = ThreadPool::new(2);
+        let (send, recv) = unbounded();
+
+        for _ in 0..4 {
+            let send = send.clone();
+
+            pool.execute(move || {
+                for _ in 0..40 {
+                    // Do some long proccess
+                }
+
+                send.send(40).unwrap();
+            })?;
+        }
+
+        assert_eq!(recv.recv().unwrap(), 40);
+        assert_eq!(recv.recv().unwrap(), 40);
+        assert_eq!(recv.recv().unwrap(), 40);
+        assert_eq!(recv.recv().unwrap(), 40);
+        assert_eq!(recv.try_recv().is_err(), true);
+
+        Ok(())
+    }
+}
